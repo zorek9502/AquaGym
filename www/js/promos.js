10f164206424d793f538c155c95051d339ac53c3
JorgePeraltaGamboa <36437763+JorgePeraltaGamboa@users.noreply.github.com>
2018-05-21 20:59:13 -0500

    Add files via upload

--- www/js/promos.js
+++ www/js/promos.js
@@ -1,8 +1,19 @@
 function getCode(id) {
+    var promos=["promo1","promo2","promo3","promo5"]
     var code = new String(document.getElementById(id).getAttribute("codigo").toString());
-    document.getElementById("cupon-modal").innerHTML = code;
     document.getElementById("inputCupon").value = code;
-    console.log();
+    if(code==""){
+        alert("entre");
+        document.getElementById("cupon-modal").innerHTML = code;
+        document.getElementById("cupon-applied").innerHTML = "";
+        document.getElementById("cupon-instructions").innerHTML = "Ésta imagen no contiene ningún cupón o promoción asociados";
+    }
+    
+    if (promos.includes(id.toString())){
+        document.getElementById("cupon-modal").innerHTML = code;
+        document.getElementById("cupon-applied").innerHTML = "Cupón aplicado.";
+        document.getElementById("cupon-instructions").innerHTML = "Encuentre el cupón aplicado en la sección de inscripciones";
+    }
     switch (id.toString()) {
         case "promo1": //GYM
             document.getElementById("sel-gym").setAttribute("selected", true);
@@ -15,6 +26,10 @@ function getCode(id) {
             break;
         case "promo5": //WATERPOLO
             document.getElementById("sel-waterpolo").setAttribute("selected", true);
+            break;
+        default:
+            alert("some");
+            document.getElementById("sel-default").selected = true;
             break;
     }
 }
